345412770b04a382645a336a0d734e6f8268b21a
ManiKumar Nakka <manikumarnakka324@gmail.com>
2024-07-30 14:38:47 +0530

    ChatsPage Updated

--- vite-project/src/ChatsPage.jsx
+++ vite-project/src/ChatsPage.jsx
@@ -17,3 +17,25 @@ const ChatsPage = (props) => {
 }
 
 export default ChatsPage;
+
+//This is the way to create a ChatsPage using the react-chat-engine-advanced library.
+
+// import React from 'react';
+// import {MultiChatWindow,MultiChatSocket,useMultiChatLogic} from 'react-chat-engine-advanced';
+
+// const ChatsPage = (props) => {
+//     const chatProps = useMultiChatLogic(
+//         '065538fc-1cd8-4f5b-841d-a7e0902eb30d',
+//         props.user.username,
+//         props.user.secret
+//       );
+    
+//   return (
+//     <div style={{height:"100vh"}}>
+//          <MultiChatWindow {...chatProps} />
+//          <MultiChatSocket {...chatProps} style={{height:"100%"}}/>
+//     </div>
+//   )
+// }
+
+// export default ChatsPage;
